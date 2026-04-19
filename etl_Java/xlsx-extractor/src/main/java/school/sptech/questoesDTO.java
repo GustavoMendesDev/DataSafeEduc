28e7f626db260ab47bf22647a0935ea1f3b36f18
Guilherme-Mesquita06 <guilherme.mesquita@sptech.school>
2026-04-19 00:51:04 -0300

    Feature: Adicionando projeto de extracao de dados, configurando ambiente para extração e inserção no banco

---
+++ etl_Java/xlsx-extractor/src/main/java/school/sptech/questoesDTO.java
@@ -0,0 +1,84 @@
+package school.sptech;
+
+import java.util.Objects;
+
+
+public class questoesDTO {
+
+    private String codigoItem;
+    private String habilidade;
+    private String respostaGabarito;
+    private String areaConhecimento;
+    private Double descriminacao;
+    private Double dificuldade;
+    private Double probabilidadeAcertos;
+
+
+    public questoesDTO(String codigoItem, String habilidade, String respostaGabarito,
+                       String areaConhecimento, Double descriminacao, Double dificuldade,
+                       Double probabilidadeAcertos) {
+        this.codigoItem = codigoItem;
+        this.habilidade = habilidade;
+        this.respostaGabarito = respostaGabarito;
+        this.areaConhecimento = areaConhecimento;
+        this.descriminacao = descriminacao;
+        this.dificuldade = dificuldade;
+        this.probabilidadeAcertos = probabilidadeAcertos;
+    }
+
+    public String getCodigoItem() {
+        return codigoItem;
+    }
+
+    public void setCodigoItem(String codigoItem) {
+        this.codigoItem = codigoItem;
+    }
+
+    public String getHabilidade() {
+        return habilidade;
+    }
+
+    public void setHabilidade(String habilidade) {
+        this.habilidade = habilidade;
+    }
+
+    public String getRespostaGabarito() {
+        return respostaGabarito;
+    }
+
+    public void setRespostaGabarito(String respostaGabarito) {
+        this.respostaGabarito = respostaGabarito;
+    }
+
+    public String getAreaConhecimento() {
+        return areaConhecimento;
+    }
+
+    public void setAreaConhecimento(String areaConhecimento) {
+        this.areaConhecimento = areaConhecimento;
+    }
+
+    public Double getDescriminacao() {
+        return descriminacao;
+    }
+
+    public void setDescriminacao(Double descriminacao) {
+        this.descriminacao = descriminacao;
+    }
+
+    public Double getDificuldade() {
+        return dificuldade;
+    }
+
+    public void setDificuldade(Double dificuldade) {
+        this.dificuldade = dificuldade;
+    }
+
+    public Double getProbabilidadeAcertos() {
+        return probabilidadeAcertos;
+    }
+
+    public void setProbabilidadeAcertos(Double probabilidadeAcertos) {
+        this.probabilidadeAcertos = probabilidadeAcertos;
+    }
+}
