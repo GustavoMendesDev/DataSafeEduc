9ca6f8cea0bcaf41c1d7e07d4dbd69a6e1dc8a83
Muriszk <murilo.suzuki@sptech.school>
2026-05-18 13:10:44 -0300

    Fix: corrigindo rotas

--- DataSafeEduc/src/routes/index.js
+++ DataSafeEduc/src/routes/index.js
@@ -1,9 +1,14 @@
 var express = require("express");
 var path = require("path");
 var router = express.Router();
+var path = require("path");
 
 router.get("/", function (req, res) {
+<<<<<<< HEAD
     res.sendFile(path.join(__dirname, "../../public/views/index.html"));
+=======
+    res.sendFile(path.resolve(__dirname, "../../public/views/index.html"));
+>>>>>>> 5199721 (Fix: corrigindo rotas)
 });
 
 router.get("/:pagina", function (req, res, next) {
