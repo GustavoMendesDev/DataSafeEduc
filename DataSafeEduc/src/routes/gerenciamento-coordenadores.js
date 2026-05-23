2baca31129a451503f8eae13208b12b654705bd3
Muriszk <murilo.suzuki@sptech.school>
2026-05-22 21:55:34 -0300

    Fix: removendo diretório redundantes, para poder implementar nova maneira de cadastrar coodenadores

--- DataSafeEduc/src/routes/gerenciamento-coordenadores.js
+++
@@ -1,6 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-var usuarioController = require("../controller/coordenadoresController");
-
-// Recebendo os dados do html e direcionando para as funcões de adm
